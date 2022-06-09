51adadf183158e64b918ef6d5b2942fbf8281fc9
Jouni Mietola <jouni.mietola@gmail.com>
2022-06-09 21:22:26 +0300

    Add webgl-plot

--- apps/remote/src/app/components/RemoteEntry.tsx
+++ apps/remote/src/app/components/RemoteEntry.tsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
+import WebglAppSin from "./webglAppSin";
+
 function RemoteEntry() {
+  let [freq, setFreq] = useState(0.001);
+  let [amp, setAmp] = useState(0.5);
   return (
     <div>
-      <h3>Remote Entry</h3>
+     <WebglAppSin freq={freq} amp={amp} />
+      <h3>Virtual Oscilloscope</h3>
     </div>
   );
 }
